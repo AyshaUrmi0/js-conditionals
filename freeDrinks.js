56f2ac8b29428ae4c70ee6c621517d5f711e8159
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-09-05 09:46:10 +0900

    free drinks done

---
+++ freeDrinks.js
@@ -0,0 +1,21 @@
+
+
+
+
+let burgerPrice=500;
+
+if(burgerPrice>500){
+    console.log("free coke");
+}else{
+    console.log(" coke: 30tk");
+}
+
+
+
+let burgerprice2=550;
+
+if(burgerprice2>500){
+    console.log("free coke");
+}else{
+    console.log(" coke: 30tk");
+}
